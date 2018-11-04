95865c6cc7925113bcf89272db3229543f225ca9
Sam Kim <samuelkim523@gmail.com>
2018-11-04 13:26:42 -0500

    Refactored project structure

--- src/MusicShare_1/music_share_app/hot_spot_music_share/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HotSpotMusicShareConfig(AppConfig):
-    name = 'hot_spot_music_share'
---
+++ src/webapps/hot_pot_music_share/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from hot_pot_music_share import views
+from django.contrib.auth.views import LoginView, logout_then_login
+
+# urlpatterns = [
+#     path('', views.global_stream, name='global_stream'),
+#     path('login', LoginView.as_view(template_name="grumblr/login.html"), name='login'),
+#     path('logout', logout_then_login, name='logout'),
+#     path('register', views.register, name='register'),
+# ]
